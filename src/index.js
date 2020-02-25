e45f08a8a870dbf3e4a64890c4f45ebcf75c08a1
Dominique Skaf <doskaf@yahoo.com>
2020-02-25 19:11:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function colorBreeds() {
 
 function dropDown() {
   let breeds = document.getElementById("dog-breeds").querySelectorAll("li");
-  document.getElementById("breed-dropdown").addEventListener("mouseover", function() {
+  document.getElementById("breed-dropdown").addEventListener("change", function() {
     breeds.forEach(breed => {
       if (breed.innerText[0] !== document.getElementById("breed-dropdown").value) {
         breed.style.display = "none"
